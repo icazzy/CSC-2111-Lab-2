a6b065fba7ad4ee362e89e67479a39e8d5b4de75
Ian Cassetty <icazzy@gmail.com>
2014-09-04 14:06:10 -0500

    changed 1

--- WriteFile.h
+++ WriteFile.h
@@ -6,15 +6,18 @@
 #include <fstream>
 using namespace std;
 
-struct WriteFile
+class WriteFile
 {
+   private;   
    ofstream output_file;
    bool closed;
+   
+   public;
+   WriteFile(const char* file_name);
+   ~WriteFile();
+   String* writeLine();
+   bool eof();
+   void close();
 };
-
-WriteFile* createWriteFile(const char* file_name);
-void destroyWriteFile(WriteFile* wf);
-void writeLine(WriteFile* wf, String* line);
-void close(WriteFile* wf);
 
 #endif
