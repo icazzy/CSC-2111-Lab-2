b607a40b205845055cd79439a4332a306ddf7a08
Zeejfps <Zeejfps@gmail.com>
2014-09-04 14:15:42 -0500

    Converted ReadFile struct to Class

--- ReadFile.h
+++ ReadFile.h
@@ -9,9 +9,9 @@ using namespace std;
 class ReadFile {
 
 private:
-   ifstream m_input_file;
-   bool m_eof;
-   bool m_closed;
+   	ifstream m_input_file;
+   	bool m_eof;
+   	bool m_closed;
 
 public:
 	ReadFile(const char* file_name);
